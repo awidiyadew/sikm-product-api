cce14cab981c5ca00881baf600997606f393b515
@dewa.awidiya <dewa.awidiya@shopee.com>
2023-06-01 12:04:58 +0700

    complete session 1

--- model/dto.go
+++ model/dto.go
@@ -1,9 +1,16 @@
 package model
 
 type ProductDetail struct {
-	ID         uint     `json:"id"`
-	Name       string   `json:"name"`
-	Price      int      `json:"price"`
-	User       User     `json:"poster"`
-	Category   Category `json:"category"`
+	ID       uint     `json:"id"`
+	Name     string   `json:"name"`
+	Price    int      `json:"price"`
+	User     User     `json:"poster"`
+	Category Category `json:"category"`
+}
+
+type ProductRequest struct {
+	Name       string `json:"name" binding:"required"`
+	Price      int    `json:"price" binding:"required,gt=0"`
+	CategoryID int    `json:"category_id" binding:"required"`
+	PostedBy   int    `json:"posted_by" binding:"required"`
 }
--- service/product.go
+++ service/product.go
@@ -1,14 +1,25 @@
 package service
 
 import (
+	"product-api/apperror"
 	"product-api/model"
 	"product-api/repository"
+	"strings"
+)
+
+var (
+	blackListedWords = []string{
+		"termurah",
+		"terbaik",
+		"diskon",
+		"promo",
+	}
 )
 
 type ProductService interface {
 	GetList() ([]model.Product, error)
 	GetByID(id int) (*model.ProductDetail, error)
-	Store(*model.Product) error
+	Store(*model.ProductRequest) error
 	Delete(*model.Product) error
 	Update(*model.Product) error
 }
@@ -24,19 +35,45 @@ func NewProductService(prdRepo repository.ProductRepository) ProductService {
 }
 
 func (s *productServiceImpl) GetList() ([]model.Product, error) {
-	// TODO: add code
-	return []model.Product{}, nil
+	return s.productRepo.FindAll()
 }
 
 func (s *productServiceImpl) GetByID(id int) (*model.ProductDetail, error) {
-	// TODO: add code
-	// TODO: use DTO to transform data from model
-	return nil, nil
+	product, err := s.productRepo.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.ProductDetail{
+		ID:       product.ID,
+		Name:     product.Name,
+		Price:    product.Price,
+		User:     product.User,
+		Category: product.Category,
+	}, nil
 }
 
-func (s *productServiceImpl) Store(product *model.Product) error {
-	// TODO: add code
-	return nil
+func (s *productServiceImpl) isValidName(name string) bool {
+	for _, word := range blackListedWords {
+		if strings.Contains(strings.ToLower(name), word) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *productServiceImpl) Store(payload *model.ProductRequest) error {
+	if !s.isValidName(payload.Name) {
+		return apperror.ErrInvalidProductName
+	}
+
+	product := model.Product{
+		Name:       payload.Name,
+		Price:      payload.Price,
+		CategoryID: payload.CategoryID,
+		PostedBy:   uint(payload.PostedBy),
+	}
+	return s.productRepo.Insert(&product)
 }
 
 func (s *productServiceImpl) Update(*model.Product) error {
